6ae630751e36983e778c502b0e081f6aedb0cf65
Jenner LaFave <jenner@jfave.com>
2012-08-08 05:52:09 -0700

    Added startup script

---
+++ start.rb
@@ -0,0 +1,6 @@
+require 'daemons'
+pwd = Dir.pwd
+Daemons.run_proc('octavia.rb', { :dir_mode => :normal, :dir => "#{pwd}/pids" }) do
+    Dir.chdir pwd
+    exec "ruby octavia.rb -p 3001 -e production"
+end
